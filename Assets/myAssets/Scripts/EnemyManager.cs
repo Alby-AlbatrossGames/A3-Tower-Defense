11485307a39a86916afd19fa9b1f178c879ac158
alby-cy <droper1@myjmc.edu.au>
2024-08-09 13:43:56 +1000

    Horde/Wave Spawning

--- Assets/myAssets/Scripts/EnemyManager.cs
+++ Assets/myAssets/Scripts/EnemyManager.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using UnityEngine;
+using System.Collections;
 
 public enum GameMode //put in GameManager script
 {
@@ -14,4 +15,31 @@ public class EnemyManager : Singleton<EnemyManager>
     public GameObject[] EnemyPrefabs;
     public Transform[] NodeList;
     public List<GameObject> ActiveEnemyList;
+
+    void SpawnEnemy(int _type)
+    {
+        Instantiate(EnemyPrefabs[_type]);
+        Debug.Log("Spawned!");
+    }
+
+    public IEnumerator SpawnWave(int _hordeCount, int _hordeSize, int _type)
+    {
+        for (int i = 0; i < _hordeCount; i++)
+        {
+            StartCoroutine(SpawnHorde(_hordeSize, _type));
+            Debug.Log("SpawnWave");
+            yield return new WaitForSeconds(Random.Range(0.5f,1.5f)); //fix time
+        }
+        
+    }
+
+    public IEnumerator SpawnHorde(int _count, int _type)
+    {
+        for (int i = 0; i < _count; i++)
+        {
+            SpawnEnemy(_type);
+            Debug.Log("SpawnEnemy");
+            yield return new WaitForSeconds(Random.Range(0.5f,1.5f)); //fix time
+        }
+    }
 }
--- Assets/myAssets/Scripts/GameManager.cs
+++ Assets/myAssets/Scripts/GameManager.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public enum GameState
@@ -9,4 +10,25 @@ public enum GameState
 public class GameManager : Singleton<GameManager>
 {
     public GameState gState;
+    public int curWave = 0;
+
+    private void Start()
+    {
+        curWave = 1;
+    }
+
+    public void StartWave(int _wave)
+    {
+        StartCoroutine(_EM.SpawnWave(_wave, _wave + 2, 0));
+        curWave++;
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            StartWave(curWave);
+            Debug.Log("Esc");
+        }
+            
+    }
 }
